f78d8e9a739d4508743de928927e4f41dbe5eed8
Orlando Flores <orlando_flores@id.uff.br>
2021-04-17 15:10:14 -0300

    feature stats adicionada -  funçao showStats faz mt coisa da pra melhorar

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const API_URL_ID = 'https://akabab.github.io/superhero-api/api/id/';
 
 const fetchHeroesInfos = async (url) => {
   try {
-    const response =  await fetch(url);
+    const response = await fetch(url);
     const data = await response.json();
     return data;
   } catch (error) {
@@ -30,32 +30,87 @@ const getHeroesIds = async () => {
 
 const appendNamesOnSelect = async () => {
   const namesArray = await getHeroesNames();
-  const select = document.querySelector('#heroes-list');
+  const selects = document.querySelectorAll('.heroes-list');
   namesArray.forEach((name) => {
-    const option = document.createElement('option');
-    option.innerText = name; 
-    option.value = name;
-    select.appendChild(option);
+    selects.forEach((select) => {
+      const option = document.createElement('option');
+      option.innerText = name;
+      option.value = name;
+      select.appendChild(option);
+    })
   });
-  getSelectedHeroe();
+  // getSelectedHeroe();
 }
 
 const getSelectedHeroe = () => {
-  const select = document.querySelector('select');
-  select.addEventListener('change', (event) => {
-    const heroe = event.target.value;
-    GetHeroeStats(heroe);
-  });
+  const selects = document.querySelector('select');
+  selects.forEach((select) => {
+    select.addEventListener('change', (event) => {
+      const heroe = event.target.value;
+      return heroe;
+    });
+  })
 }
 
-const GetHeroeStats = async (heroe) => {
-  const ids = await getHeroesIds();
-  const heroeId = ids.find((id) => id[heroe])[heroe];
-  const infos = await fetchHeroesInfos(`${API_URL_ID}${heroeId}.json`);
-  console.log(infos);
-  const stats = infos.powerstats;
-  const imgXsUrl = infos.images.xs;
+const getHeroeStats = async (heroe) => {
+  try {
+    const ids = await getHeroesIds();
+    const heroeId = ids.find((id) => id[heroe])[heroe];
+    const infos = await fetchHeroesInfos(`${API_URL_ID}${heroeId}.json`);
+    const stats = infos.powerstats;
+    // console.log(stats);
+    return stats;
+    const imgXsUrl = infos.images.xs;
+    // console.log(infos);
+  } catch (error) {
+    alert('Selecione um herói');
+  }
+}
+
+const getHeroePhoto = async (heroe) => {
+  try {
+    const ids = await getHeroesIds();
+    const heroeId = ids.find((id) => id[heroe])[heroe];
+    const infos = await fetchHeroesInfos(`${API_URL_ID}${heroeId}.json`);
+    // const stats = infos.powerstats;
+    // console.log(stats);
+    const imgXsUrl = infos.images.sm;
+    return imgXsUrl;
+    // console.log(infos);
+  } catch (error) {
+    alert('Selecione um herói');
+  }
+}
+
+const showStats = async () => {
+  const btnStats = document.getElementById('stats');
+  const heroe = document.querySelector('#heroe-list');
+  const opponent = document.querySelector('#opponent-list');
+  const heroePhoto = document.querySelector('#heroe-photo');
+  const opponentPhoto = document.querySelector('#opponent-photo');
+  btnStats.addEventListener('click', async () => {
+    const heroeURL = await getHeroePhoto(heroe.value);
+    const opponentURL = await getHeroePhoto(opponent.value);
+    const heroeStats = await getHeroeStats(heroe.value);
+    const opponentStats = await getHeroeStats(opponent.value);
+    const keys = Object.keys(heroeStats);
+    const heroeUl = document.getElementById('heroe-ul');
+    heroeUl.innerText = '';
+    const opponentUl = document.getElementById('opponent-ul');
+    opponentUl.innerText = '';
+    for (key of keys) {
+      const liHeroe = document.createElement('li');
+      const liOpponent = document.createElement('li');
+      liHeroe.innerText = `${key} : ${heroeStats[key]}`;
+      liOpponent.innerText = `${key} : ${opponentStats[key]}`;
+      heroeUl.appendChild(liHeroe);
+      opponentUl.appendChild(liOpponent);
+    }
+    heroePhoto.src = heroeURL;
+    opponentPhoto.src = opponentURL;
+  })
 }
 window.onload = () => {
   appendNamesOnSelect();
+  showStats();
 }
